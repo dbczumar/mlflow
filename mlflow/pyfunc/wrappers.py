3a3e75937ff7bee833faa299d5bb7adad830ca4f
Corey Zumar <corey.zumar@databricks.com>
2018-11-20 16:10:02 -0800

    Model save load

--- mlflow/pyfunc/wrappers.py
+++ mlflow/pyfunc/wrappers.py
@@ -32,93 +32,13 @@ class BaseModelWrapper(object):
         self.run_id = run_id
 
 
-class ExtendedModel(BaseModelWrapper):
-
-    def __init__(self, parent_model, predict_fn, parent_model_path, parent_run_id):
-        super(ExtendedModel, self).__init__()
-        self.parent_model = parent_model
-        self.predict_fn = predict_fn
-        self.parent_model_path = parent_model_path
-        self.parent_run_id = parent_run_id
-
-    def predict(self, input_data):
-        return self.predict_fn(self.parent_model, input_data)
-
-    @property
-    def base_flavor(self):
-        return mlflow.pyfunc.FLAVOR_NAME
-
-    @property
-    def base_model(self):
-        return self.parent_model
-
-    def get_parent_model_path(self):
-        """
-        Docs
-        """
-        return self.parent_model_path
-
-    def get_parent_run_id(self):
-        return self.parent_run_id
-
-    # def save(self, path, code=None, conda_env=None):
-    #     path = os.path.abspath(path)
-    #     if os.path.exists(path):
-    #         raise Exception
-    #
-    #     if run_id is not None:
-    #         base_model_path = tracking.utils._get_model_log_dir(base_model_path, run_id)
-    #
-    #     base_pyfunc_conf = _get_flavor_configuration(
-    #             model_path=base_model_path, flavor_name=FLAVOR_NAME)
-    #
-    #     os.makedirs(path)
-    #     base_model_subpath = _copy_file_or_tree(src=base_model_path, dst=path, dst_dir="base_model")
-    #     base_model_path = os.path.join(path, base_model_subpath)
-    #
-    #     if conda_env is None and ENV in base_pyfunc_conf:
-    #         conda_env = os.path.join(base_model_path, ENV)
-    #     if conda_env is not None:
-    #         conda_env_subpath = "conda_env.yaml"
-    #         shutil.copy(src=conda_env, dst=os.path.join(path, conda_env_subpath))
-    #     else:
-    #         conda_env_subpath = None
-    #
-    #     if code is not None:
-    #         code_subpath = "code"
-    #         for code_path in code:
-    #             _copy_file_or_tree(src=code_path, dst=path, dst_dir=code_subpath)
-    #     else:
-    #         code_subpath = None
-    #
-    #     model_conf = Model()
-    #     add_to_model(model=model_conf, loader_module="mlflow.pyfunc.wrapped_model", 
-    #                  env=conda_env_subpath, code=code_subpath, data=None)
-    #     model_conf.save(os.path.join(path, "MLmodel"))
-
-    
-    # def log(
-    #
-
-
-# def _load_pyfunc(model_root_path):
-#     pyfunc_conf = _get_flavor_configuration(model_root_path, mlflow.pyfunc.FLAVOR_NAME) 
-#
-#     parent_model_path = os.path.join(model_root_path, pyfunc_conf["parent_model"])
-#     parent_model = mlflow.pyfunc.load_pyfunc(path=parent_model_path)
-#
-#     predict_fn_path = os.path.join(model_root_path, pyfunc_conf["predict_fn"])
-#     with open(predict_fn_path, "rb") as f:
-#         predict_fn = cloudpickle.load(f)
-#
-#     return PyfuncModel(parent_model=parent_model, predict_fn=predict_fn)
-
-
 def extend_model(model_path, run_id=None):
     return ModelExtender(model_path, run_id)
 
 import os
+import shutil
 
+from mlflow.models import Model
 from mlflow.tracking.utils import _get_model_log_dir
 from mlflow.utils.file_utils import TempDir 
 from mlflow.utils.file_utils import _copy_file_or_tree 
@@ -139,7 +59,8 @@ class ModelExtender:
         self.pyfunc_model = None
         
         self.entered = False 
-        self.model_dir = TempDir()
+        self.tmp_model_dir = TempDir()
+        self.tmp_model_path = None
 
         self.predict_fn = None
         self.data = {}
@@ -158,15 +79,16 @@ class ModelExtender:
         self.predict_fn = lambda model, data, input_data: self.pyfunc_model.predict(input_data)
 
         # Create a temporary directory housing model artifacts
-        self.model_dir.__enter__()
-        _copy_file_or_tree(src=self.model_path, dst=self.model_dir.path(), dst_dir="base_model")
+        self.tmp_model_dir.__enter__()
+        tmp_model_base_path = self.tmp_model_dir.path()
+        tmp_model_subpath = _copy_file_or_tree(src=self.model_path, dst=tmp_model_base_path, dst_dir="")
+        self.tmp_model_path = os.path.join(tmp_model_base_path, tmp_model_subpath)
 
         return self
 
-
     def __exit__(self, *args, **kwargs):
         ModelExtender._contexts -= 1
-        self.model_dir.__exit__(*args, **kwargs)
+        self.tmp_model_dir.__exit__(*args, **kwargs)
 
     def set_predict(self, predict_fn):
         self.predict_fn = predict_fn
@@ -178,27 +100,29 @@ class ModelExtender:
         if not self.predict_fn:
             raise Exception
         
-        return predict_fn(self.model, self.data, input_data)
+        return self.predict_fn(self.model, self.data, input_data)
 
     def save(self, path, code=None, conda_env=None):
         path = os.path.abspath(path)
         if os.path.exists(path):
             raise Exception
 
-        base_pyfunc_conf = _get_flavor_configuration(
-                model_path=base_model_path, flavor_name=FLAVOR_NAME)
+        parent_pyfunc_conf = _get_flavor_configuration(
+                model_path=self.tmp_model_path, flavor_name=mlflow.pyfunc.FLAVOR_NAME)
 
         os.makedirs(path)
-        base_model_subpath = _copy_file_or_tree(src=base_model_path, dst=path, dst_dir="base_model")
-        base_model_path = os.path.join(path, base_model_subpath)
+        parent_model_subpath = _copy_file_or_tree(
+                src=self.tmp_model_path, dst=path, dst_dir="base_model")
+        parent_model_path = os.path.join(path, parent_model_subpath)
 
-        if conda_env is None and ENV in base_pyfunc_conf:
-            conda_env = os.path.join(base_model_path, ENV)
+        if conda_env is None and mlflow.pyfunc.ENV in parent_pyfunc_conf:
+            conda_env = os.path.join(parent_model_path, mlflow.pyfunc.ENV)
         if conda_env is not None:
             conda_env_subpath = "conda_env.yaml"
             shutil.copy(src=conda_env, dst=os.path.join(path, conda_env_subpath))
         else:
             conda_env_subpath = None
+
 
         if code is not None:
             code_subpath = "code"
@@ -208,8 +132,9 @@ class ModelExtender:
             code_subpath = None
 
         model_conf = Model()
-        add_to_model(model=model_conf, loader_module="mlflow.pyfunc.wrapped_model", 
-                     env=conda_env_subpath, code=code_subpath, data=None)
+        mlflow.pyfunc.add_to_model(
+                model=model_conf, loader_module=__name__, 
+                env=conda_env_subpath, code=code_subpath, data=None)
         model_conf.save(os.path.join(path, "MLmodel"))
 
     def log(self, artifact_path):
