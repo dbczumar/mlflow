d0aacbb46af73b46f25d31bdeedb7aa08c8b5a59
Corey Zumar <corey.zumar@databricks.com>
2019-01-07 18:36:35 -0800

    Change

--- examples/flower_classifier/image_pyfunc.py
+++ examples/flower_classifier/image_pyfunc.py
@@ -3,6 +3,7 @@ Example of a custom python function implementing image classifier with image pre
 in the model.
 """
 import base64
+import cloudpickle
 from io import BytesIO
 import keras
 import numpy as np
@@ -31,46 +32,20 @@ def decode_and_resize_image(raw_bytes, size):
     return np.asarray(Image.open(BytesIO(raw_bytes)).resize(size), dtype=np.float32)
 
 
-class KerasImageClassifierPyfunc(object):
-    """
-    Image classification model with embedded pre-processing.
+class KerasImageClassifier2(mlflow.pyfunc.PythonModel):
 
-    This class is essentially an MLflow custom python function wrapper around a Keras model.
-    The wrapper provides image preprocessing so that the model can be applied to images directly.
-    The input to the model is base64 encoded image binary data (e.g. contents of a jpeg file).
-    The output is the predicted class label, predicted class id followed by probabilities for each
-    class.
-
-    The model declares current local versions of Keras, Tensorlow and pillow as dependencies in its
-    conda environment file.
-    """
-
-    def __init__(self, graph, session, model, image_dims, domain):
-        self._graph = graph
-        self._session = session
-        self._model = model
-        self._image_dims = image_dims
-        self._domain = domain
-        probs_names = ["p({})".format(x) for x in domain]
+    def __init__(self, context):
+        super(KerasImageClassifier2, self).__init__(context)
+        self._tf_graph = tf.Graph()
+        self._tf_sess = tf.Session(graph=self._tf_graph)
+        probs_names = ["p({})".format(x) for x in context.parameters["domain"]]
         self._column_names = ["predicted_label", "predicted_label_id"] + probs_names
+        with self._tf_graph.as_default():
+            keras.backend.set_session(self._tf_sess)
+            self._keras_model = mlflow.keras.load_model(context.artifacts["keras_model"])
 
-    def predict(self, input):
-        """
-        Generate predictions for the data.
 
-        :param input: pandas.DataFrame with one column containing images to be scored. The image
-                     column must contain base64 encoded binary content of the image files. The image
-                     format must be supported by PIL (e.g. jpeg or png).
-
-        :return: pandas.DataFrame containing predictions with the following schema:
-                     Predicted class: string,
-                     Predicted class index: int,
-                     Probability(class==0): float,
-                     ...,
-                     Probability(class==N): float,
-        """
-
-        # decode image bytes from base64 encoding
+    def predict(self, model_input):
         def decode_img(x):
             return pd.Series(base64.decodebytes(bytearray(x[0], encoding="utf8")))
 
@@ -78,27 +53,23 @@ class KerasImageClassifierPyfunc(object):
         probs = self._predict_images(images)
         m, n = probs.shape
         label_idx = np.argmax(probs, axis=1)
-        labels = np.array([self._domain[i] for i in label_idx], dtype=np.str).reshape(m, 1)
+        labels = np.array([
+            self.context.parameters["domain"][i] for i in label_idx], dtype=np.str).reshape(m, 1)
         output_data = np.concatenate((labels, label_idx.reshape(m, 1), probs), axis=1)
         res = pd.DataFrame(columns=self._column_names, data=output_data)
         res.index = input.index
         return res
 
-    def _predict_images(self, images):
-        """
-        Generate predictions for input images.
-        :param images: binary image data
-        :return: predicted probabilities for each class
-        """
 
+    def _predict_images(self, images):
         def preprocess_f(z):
-            return decode_and_resize_image(z, self._image_dims[:2])
+            return decode_and_resize_image(z, self.parameters["image_dims"][:2])
 
         x = np.array(
             images[images.columns[0]].apply(preprocess_f).tolist())
-        with self._graph.as_default():
-            with self._session.as_default():
-                return self._model.predict(x)
+        with self._tf_graph.as_default():
+            with self._tf_sess.as_default():
+                return self._keras_model.predict(x)
 
 
 def log_model(keras_model, artifact_path, image_dims, domain):
@@ -112,48 +83,29 @@ def log_model(keras_model, artifact_path, image_dims, domain):
     """
 
     with TempDir() as tmp:
-        data_path = tmp.path("image_model")
-        os.mkdir(data_path)
-        conf = {
-            "image_dims": "/".join(map(str, image_dims)),
-            "domain": "/".join(map(str, domain))
-        }
-        with open(os.path.join(data_path, "conf.yaml"), "w") as f:
-            yaml.safe_dump(conf, stream=f)
-        keras_path = os.path.join(data_path, "keras_model")
-        mlflow.keras.save_model(keras_model, path=keras_path)
+        keras_model_path = tmp.path("keras_model")
+        mlflow.keras.save_model(keras_model, path=keras_model_path)
+
         conda_env = tmp.path("conda_env.yaml")
         with open(conda_env, "w") as f:
             f.write(conda_env_template.format(python_version=PYTHON_VERSION,
+                                              cloudpickle_version=cloudpickle.__version__,
                                               keras_version=keras.__version__,
                                               tf_name=tf.__name__,  # can have optional -gpu suffix
                                               tf_version=tf.__version__,
                                               pillow_version=PIL.__version__))
 
-        mlflow.pyfunc.log_model(artifact_path=artifact_path,
-                                loader_module=__name__,
-                                code_path=[__file__],
-                                data_path=data_path,
-                                conda_env=conda_env)
-
-
-def _load_pyfunc(path):
-    """
-    Load the KerasImageClassifierPyfunc model.
-    """
-    with open(os.path.join(path, "conf.yaml"), "r") as f:
-        conf = yaml.safe_load(f)
-    keras_model_path = os.path.join(path, "keras_model")
-    domain = conf["domain"].split("/")
-    image_dims = np.array([int(x) for x in conf["image_dims"].split("/")], dtype=np.int32)
-    # NOTE: Tensorflow based models depend on global state (Graph and Session) given by the context.
-    # To make sure we score the model in the same session as we loaded it int, we create a new
-    # session and a new graph here and store them with the model.
-    with tf.Graph().as_default() as g:
-        with tf.Session().as_default() as sess:
-            keras.backend.set_session(sess)
-            keras_model = mlflow.keras.load_model(keras_model_path)
-    return KerasImageClassifierPyfunc(g, sess, keras_model, image_dims, domain=domain)
+        mlflow.pyfunc.log_model(
+            artifact_path=artifact_path,
+            model_class=KerasImageClassifier2,
+            artifacts={
+                "keras_model": keras_model_path,
+            },
+            parameters={
+                "image_dims": image_dims,
+                "domain": domain,
+            },
+            code_paths=[__file__])
 
 
 conda_env_template = """        
@@ -163,7 +115,8 @@ channels:
   - anaconda
 dependencies:
   - python=={python_version}
-  - keras=={keras_version}  
+  - cloudpickle=={cloudpickle_version}
+  - keras=={keras_version} 
   - {tf_name}=={tf_version} 
   - pip:    
     - pillow=={pillow_version}
