52d8e2d05eda59e2c6aa0225bc2faafed0267caf
Corey Zumar <corey.zumar@databricks.com>
2020-01-22 14:38:17 -0800

    LR

--- mlflow/keras.py
+++ mlflow/keras.py
@@ -435,11 +435,11 @@ def autolog():
         def on_train_begin(self, logs=None):  # pylint: disable=unused-argument
             # try_mlflow_log(mlflow.log_param, 'num_layers', len(self.model.layers))
             try_mlflow_log(mlflow.log_param, 'optimizer_name', type(self.model.optimizer).__name__)
-            if hasattr(self.model.optimizer, 'lr'):
-                lr = self.model.optimizer.lr if \
-                    type(self.model.optimizer.lr) is float \
-                    else keras.backend.eval(self.model.optimizer.lr)
-                # try_mlflow_log(mlflow.log_param, 'learning_rate', lr)
+            # if hasattr(self.model.optimizer, 'lr'):
+            #     lr = self.model.optimizer.lr if \
+            #         type(self.model.optimizer.lr) is float \
+            #         else keras.backend.eval(self.model.optimizer.lr)
+            #     try_mlflow_log(mlflow.log_param, 'learning_rate', lr)
             if hasattr(self.model.optimizer, 'epsilon'):
                 epsilon = self.model.optimizer.epsilon if \
                     type(self.model.optimizer.epsilon) is float \
