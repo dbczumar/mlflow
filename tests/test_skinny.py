dfcf02e9bb04396cca3591031570cd23476fcffd
Eduardo de Leon <eddeleon@microsoft.com>
2020-11-13 11:59:12 -0500

    Format test_skinny
    Signed-off-by: Eduardo de Leon <eddeleon@microsoft.com>

--- tests/test_skinny.py
+++ tests/test_skinny.py
@@ -3,10 +3,12 @@ import os
 
 
 @pytest.mark.skipif(
-    "MLFLOW_SKINNY" not in os.environ, reason="This test is only valid for the skinny client"
+    "MLFLOW_SKINNY" not in os.environ,
+    reason="This test is only valid for the skinny client",
 )
 def test_fails_sqlalchemy_import():
     import mlflow
+
     assert mlflow is not None  # pylint or flake8 disabling is not working
     with pytest.raises(ImportError):
         import sqlalchemy  # pylint: disable=unused-import
