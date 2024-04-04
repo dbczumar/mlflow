c53756328e87ffe19106d52eb2655a1a85c83888
dbczumar <corey.zumar@databricks.com>
2024-04-03 19:44:41 -0700

    fix
    Signed-off-by: dbczumar <corey.zumar@databricks.com>

--- mlflow/utils/rest_utils.py
+++ mlflow/utils/rest_utils.py
@@ -192,7 +192,6 @@ def extract_api_info_for_service(service, path_prefix):
     service_methods = service.DESCRIPTOR.methods
     res = {}
     for service_method in service_methods:
-        print("SERVICE METHOD", service().GetRequestClass(service_method))
         endpoints = service_method.GetOptions().Extensions[databricks_pb2.rpc].endpoints
         endpoint = endpoints[0]
         endpoint_path = _get_path(path_prefix, endpoint.path)
